2781310753f0ec71b8e143b04996247de222243c
Dhananjay Raghuwanshi <dhananjay.raghuwanshi@oracle.com>
2023-06-25 08:50:52 +0530

    added jenkins file

--- sample.py
+++ sample.py
@@ -1,1 +1,18 @@
-print('this is just a sample file to start working with jenkins')
+import jenkins
+import json
+import os
+
+
+host='http://localhost:8080'
+username='Dhananjay'
+password='11140211df0da837b9dca5dac1ca0f4841'
+server=jenkins.Jenkins(host,username=username,password=password,timeout=10)
+
+# user=server.get_whoami()
+# print(user['id'])
+
+jobs=server.get_all_jobs()
+print(jobs)
+
+
+
